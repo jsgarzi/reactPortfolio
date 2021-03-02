d180f4269eb651059c48fff61db4cfa9774ec6c5
jsgarzi <jsgarzi@fortitudedevelopment.com>
2021-03-02 17:57:56 -0500

    cards on home

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Cards(props) {
+    return (
+        <div className="container fluid">
+            <div className="card" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                    <h5 className="card-title">{props.cardInfo.title}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,61 @@
 import React from 'react'
+import Cards from "../components/Cards/Cards";
+
+var cardInfo = [
+    {
+        image : "",
+        title : "Potentially A Blog",
+        discrip : "",
+        live : "",
+        git : ""
+    },
+    {
+        image : "",
+        title : "Travel God",
+        discrip : "",
+        live : "",
+        git : ""
+    },
+    {
+        image : "",
+        title : "Weather Track",
+        discrip : "",
+        live : "",
+        git : ""
+    },
+    {
+        image : "",
+        title : "Employee Directory",
+        discrip : "",
+        live : "",
+        git : ""
+    },
+    {
+        image : "",
+        title : "Fortitude Dev",
+        discrip : "",
+        live : "",
+        git : ""
+    },
+    {
+        image : "",
+        title : "Workout Tracker",
+        discrip : "",
+        live : "",
+        git : ""
+    }
+]
 
 function Home() {
     return (
         <div>
+            {cardInfo.map((data) => {
+               return (
+                < Cards 
+                        cardInfo = {data}
+                /> 
+               )
+            })}
             
         </div>
     )
