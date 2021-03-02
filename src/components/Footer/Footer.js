80fbafd4d64ecd2654b6eb50ee9c084192d5f618
jsgarzi <jsgarzi@fortitudedevelopment.com>
2021-03-02 14:27:37 -0500

    footer frame

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer (props) {
+    return (
+        <footer className = "footer">
+            <div className = "containter">
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(props) {
             <nav className="navbar navbar-dark bg-dark">
                 <div className="container-fluid ml-5 ">
                     <a className="navbar-brand" href="#">Julian Sgarzi</a>
-                    <div className= " row ml-auto mr-5">
+                    <div className= " row ml-auto mr-5 font-weight-bold">
                         <div className="navbar-nav ml-5 mb-2 mb-lg-0">
                             <a className="nav-link active" aria-current="page" onClick={props.sort}>Home</a>
                         </div>
